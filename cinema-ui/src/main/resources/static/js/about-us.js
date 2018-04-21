3f9e57a44c95c2e8077b0a3e0f61d041437e9159
Darina77 <darina.pishchik@gmail.com>
2018-04-21 21:55:54 +0300

    Add about us page

--- cinema-ui/src/main/resources/static/js/about-us.js
+++ cinema-ui/src/main/resources/static/js/about-us.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
     $(".bottom-menu .nav-link").removeClass("active");
-    $("#about-us").addClass("active");
+    $(".about-us").addClass("active");
 });
+
+function initCinemaMap() {
+    var uluru = {lat: 50.464487, lng:  30.519826};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
